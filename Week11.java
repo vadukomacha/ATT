bd286ccef8e4844f600a0697558fe0259cfd9394
vadukomacha <vadukomacha9@gmail.com>
2026-03-30 00:16:54 +0530

    Create Week11.java

---
+++ Week11.java
@@ -0,0 +1,54 @@
+package att;
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Week11 {
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver=new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.myntra.com/");
+
+		Thread.sleep(1000);
+
+		WebElement profile=driver.findElement(By.xpath("//span[normalize-space()='Profile']"));
+
+		profile.click();
+
+		Thread.sleep(1000);
+
+		WebElement login=driver.findElement(By.xpath("//a[normalize-space()='login / Signup']"));
+
+		login.click();
+
+		Thread.sleep(1000);
+
+		WebElement mobileno=driver.findElement(By.xpath("//input[@class='form-control mobileNumberInput']"));
+
+		mobileno.sendKeys("9160510123");
+
+		Thread.sleep(1000);
+
+		WebElement check=driver.findElement(By.xpath("//input[@class='consentCheckbox']"));
+
+		check.click();
+
+		Thread.sleep(1000);
+
+		WebElement proceed=driver.findElement(By.xpath("//div[normalize-space()='CONTINUE']"));
+
+		proceed.click();
+
+		Thread.sleep(1000);
+
+
+
+
+}
+}
