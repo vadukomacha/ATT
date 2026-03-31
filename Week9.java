08b4aa2fdecad76d776f42c80b101f1a27685a06
vadukomacha <vadukomacha9@gmail.com>
2026-03-31 12:49:41 +0530

    Update Week9.java

--- Week9.java
+++ Week9.java
@@ -10,7 +10,7 @@ public class Week9 {
 public static void main(String[] args) {
 WebDriver driver=new ChromeDriver();
 driver.manage().window().maximize();
-driver.get("https://www.justdial.com/Hyderabad/Bakeries/nct-10033880?trkid=11687-hyderabad-fcat-catne&term=backery&cbflg=");
+driver.get("https://www.justdial.com/Hyderabad/Bakeries/nct-10033880");
 List<WebElement> m = driver.findElements(By.xpath("//h2[contains(@class,'resultbox_title')]"));
 for(int i = 0; i< m.size(); i++) {
 String s = m.get(i).getText();
